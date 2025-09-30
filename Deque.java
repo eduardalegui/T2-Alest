92731848978ec29d35e87e7f830a68f662b47d3e
myynxx <arthurtomazelli22@gmail.com>
2025-09-30 17:03:26 -0300

    ae

--- Deque.java
+++ Deque.java
@@ -4,6 +4,7 @@ public class Deque<T> {
     private T[] elementos; // Array que armazena os dados
     private int tamanho; // Quantidade de elementos na lista
 
+    @SuppressWarnings("unchecked")
     public Deque() {
         elementos = (T[]) new Object[10];
         tamanho = 0;
