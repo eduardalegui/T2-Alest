428d99e764f717f537521c987af4b30e8669579a
PedroHCastilhos <pedro.castilhos.fraga@gmail.com>
2025-10-01 17:25:25 -0300

    Implementei o método de inserir na primeira posição

--- Deque.java
+++ Deque.java
@@ -9,4 +9,16 @@ public class Deque<T> {
         tamanho = 0;
     }
 
+    public boolean InsercaoPrimeiraPosisao(T dado) {
+        if(tamanho == 145) {
+            return false;
+        }
+        for(int i = 0; i <= tamanho; i++) {
+            T d = elementos[i];
+            elementos[0] = dado;
+            elementos[i + 1] = d;
+        }
+        tamanho++;
+        return true;
+    }
 }
